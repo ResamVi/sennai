433071bf80c5baa71718a27d3d556fcf72c02271
Julien Midedji <Julien.Midedji@student.kit.edu>
2020-12-14 20:45:19 +0100

    Move a pink square left and right on a fully sized canvas

--- src/game.ts
+++ src/game.ts
@@ -2,6 +2,9 @@ import 'phaser';
 
 export default class Demo extends Phaser.Scene
 {
+    player: Phaser.GameObjects.Rectangle;
+    cursors: Phaser.Types.Input.Keyboard.CursorKeys;
+
     constructor ()
     {
         super('demo');
@@ -9,38 +12,36 @@ export default class Demo extends Phaser.Scene
 
     preload ()
     {
-        this.load.image('logo', 'assets/phaser3-logo.png');
-        this.load.image('libs', 'assets/libs.png');
-        this.load.glsl('bundle', 'assets/plasma-bundle.glsl.js');
-        this.load.glsl('stars', 'assets/starfields.glsl.js');
     }
 
     create ()
     {
-        this.add.shader('RGB Shift Field', 0, 0, 800, 600).setOrigin(0);
+        this.player = this.add.rectangle(200, 200, 148, 148, 0xff00ff);
+        this.cursors = this.input.keyboard.createCursorKeys();
+    }
 
-        this.add.shader('Plasma', 0, 412, 800, 172).setOrigin(0);
-
-        this.add.image(400, 300, 'libs');
-
-        const logo = this.add.image(400, 70, 'logo');
-
-        this.tweens.add({
-            targets: logo,
-            y: 350,
-            duration: 1500,
-            ease: 'Sine.inOut',
-            yoyo: true,
-            repeat: -1
-        })
+    update()
+    {
+        if (this.cursors.left.isDown)
+        {
+            this.player.setX(this.player.x - 1);
+        }
+        else if (this.cursors.right.isDown)
+        {
+            this.player.setX(this.player.x + 1);
+        }
     }
 }
 
 const config = {
     type: Phaser.AUTO,
-    backgroundColor: '#125555',
-    width: 800,
-    height: 600,
+    backgroundColor: '#000000',
+    width: 1080,
+    height: 720,
+    scale: {
+        mode: Phaser.Scale.FIT,
+        autoCenter: Phaser.Scale.CENTER_BOTH
+    },
     scene: Demo
 };
 
