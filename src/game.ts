f1511f8e01e14790ef2a1c6c13545cf961503f47
Julien Midedji <Julien.Midedji@student.kit.edu>
2020-12-15 17:01:26 +0100

    Add a track and car, follow the player and display info

--- src/game.ts
+++ src/game.ts
@@ -1,9 +1,15 @@
 import 'phaser';
+import 'Car'
 
-export default class Demo extends Phaser.Scene
+export default class MainScene extends Phaser.Scene
 {
-    player: Phaser.GameObjects.Rectangle;
+    // TODO: Bundle all objects into worlds module
+    player: Phaser.Physics.Arcade.Sprite;
+
+    // TODO: Put into dedicated controls module
     cursors: Phaser.Types.Input.Keyboard.CursorKeys;
+
+    text: Phaser.GameObjects.Text;
 
     constructor ()
     {
@@ -12,24 +18,62 @@ export default class Demo extends Phaser.Scene
 
     preload ()
     {
+        // TODO: Put into dedicated scene for loading
+        this.load.image('car', 'assets/car.png');
+        this.load.image('grid', 'assets/grid.png');
+        this.load.image('track', 'assets/track.png');
     }
 
     create ()
     {
-        this.player = this.add.rectangle(200, 200, 148, 148, 0xff00ff);
+        this.add.image(987 * 5, 427 * 5, 'track').setScale(10);
+        
+        this.player = this.physics.add.sprite(5870, 4080, 'car');
+        this.player.setCollideWorldBounds(true);
+        this.player.setScale(0.5, 0.5);
+        
+        this.text = this.add.text(10, 10, '', { font: '16px Courier', fill: '#00ff00' });
+        this.text.setScrollFactor(0);
+
+        this.physics.world.setBounds(0, 0, 987 * 10, 427 * 10);
+
         this.cursors = this.input.keyboard.createCursorKeys();
+
+        this.cameras.main.setBounds(0, 0, 987 * 10, 427 * 10);
+        this.cameras.main.startFollow(this.player, false, 0.1, 0.1);
     }
 
     update()
     {
         if (this.cursors.left.isDown)
         {
-            this.player.setX(this.player.x - 1);
+            this.player.setX(this.player.x - 10);
         }
         else if (this.cursors.right.isDown)
         {
-            this.player.setX(this.player.x + 1);
+            this.player.setX(this.player.x + 10);
         }
+        
+        if (this.cursors.down.isDown)
+        {
+            this.player.setY(this.player.y + 10);
+        }
+        else if (this.cursors.up.isDown)
+        {
+            this.player.setY(this.player.y - 10);
+        }
+
+        this.debug();
+    }
+
+    debug()
+    {
+        this.text.setText([
+            'x: ' + this.player.x,
+            'y: ' + this.player.y,
+            'accel: ' + 0,
+            'fps: ' + this.game.loop.actualFps,
+        ]);
     }
 }
 
@@ -42,7 +86,12 @@ const config = {
         mode: Phaser.Scale.FIT,
         autoCenter: Phaser.Scale.CENTER_BOTH
     },
-    scene: Demo
+    scene: [MainScene],
+    physics: {
+        default: 'arcade',
+        arcade: { debug: true }
+    }
+
 };
 
 const game = new Phaser.Game(config);
