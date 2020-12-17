3df7e32f54b1ea86157461df2eb0ae39d683d4c5
Julien Midedji <Julien@resamvi.io>
2020-12-17 15:23:38 +0100

    Add proper acceleration

--- src/game.ts
+++ src/game.ts
@@ -3,17 +3,21 @@ import 'phaser';
 let graphics: any;
 let line: Phaser.Geom.Line;
 
+let round = function(numb) {
+    return Math.round(numb*100)/100;
+}
+
 export default class MainScene extends Phaser.Scene
 {
-    // TODO: Bundle all objects into worlds module
-    private player: Phaser.Physics.Matter.Sprite[] = [];
-
-    // TODO: Put into dedicated controls module
-    private cursors: Phaser.Types.Input.Keyboard.CursorKeys;
-
-    private text: Phaser.GameObjects.Text;
+    private player:         Phaser.Physics.Matter.Sprite[] = []; // TODO: Bundle all objects into worlds module
+    private cursors:        Phaser.Types.Input.Keyboard.CursorKeys; // TODO: Put into dedicated controls module
+    private text:           Phaser.GameObjects.Text;
     
-    private engineforce: number = 0;
+    private engineforce:    number                          = 0;
+    
+    private acceleration:   Phaser.Math.Vector2         = new Phaser.Math.Vector2(0, 0);
+    private velocity:       Phaser.Math.Vector2             = new Phaser.Math.Vector2(0, 0);
+    private position:       Phaser.Math.Vector2             = new Phaser.Math.Vector2(100, 100);
     
     /**
      * Using values from a Tesla Model S
@@ -55,7 +59,7 @@ export default class MainScene extends Phaser.Scene
         
         //this.player = this.matter.add.sprite(5870, 4080, 'car');
         for(let i = 0; i < 4; i++) {
-            this.player[i] = this.matter.add.sprite(100, 100 + i*100, 'car'); // TODO: Set friction to 0
+            this.player[i] = this.matter.add.sprite(this.position.x, this.position.y + i*100, 'car'); // TODO: Set friction to 0
             this.player[i].setAngle(90);
         }
 
@@ -117,25 +121,22 @@ export default class MainScene extends Phaser.Scene
 
     applyPhysics(time, delta)
     {
-        let oldVelocity: Phaser.Math.Vector2 = new Phaser.Math.Vector2(this.player[0].body.velocity.x, this.player[0].body.velocity.y);
-        let oldPosition: Phaser.Math.Vector2 = new Phaser.Math.Vector2(this.player[0].x, this.player[0].y);
-
-        let F_traction      = this.direction()  .scale(this.engineforce);
-        let F_drag          = oldVelocity       .scale(-this.C_drag * oldVelocity.length());
-        let F_rollresist    = oldVelocity       .scale(this.C_rr);
+        let F_traction      = this.direction()      .scale(this.engineforce);
+        let F_drag          = this.velocity.clone() .scale(-this.C_drag * this.velocity.length());
+        let F_rollresist    = this.velocity.clone() .scale(this.C_rr);
 
         let F_longitudinal  = F_traction.add(F_drag).add(F_rollresist);
 
-        let accel: Phaser.Math.Vector2      = F_longitudinal.scale(1 / this.M);         // a = F/M
-        let velocity: Phaser.Math.Vector2   = oldVelocity.add(accel.scale(delta));      // v = v + dt * a
-        let position: Phaser.Math.Vector2   = oldPosition.add(velocity.scale(delta));   // p = p + dt * v
+        this.acceleration   = F_longitudinal.scale(1 / this.M); // a = F/M
+        this.velocity.add(this.acceleration);                   // v = v + dt * a
+        this.position.add(this.velocity);                       // p = p + dt * v
         
-        this.player[0].setPosition(position.x, position.y);
+        this.player[0].setPosition(this.position.x, this.position.y);
 
-        this.debug(time, delta, accel.length());
+        this.debug(time, delta, this.acceleration, this.velocity);
     }
 
-    debug(time, delta, accel)
+    debug(time, delta, accel, vel)
     {
         let [originX, originY] = [this.player[0].getCenter().x, this.player[0].getCenter().y];
         let [targetX, targetY] = [this.player[0].getTopCenter().x, this.player[0].getTopCenter().y]
@@ -145,6 +146,12 @@ export default class MainScene extends Phaser.Scene
         graphics.strokeLineShape(line);
 
         /*
+        console.log("Accel");
+        console.log(accel);
+        
+        console.log("Velocity");
+        console.log(this.velocity);
+
         console.log("Position");
         console.log(position);
 
@@ -168,12 +175,15 @@ export default class MainScene extends Phaser.Scene
         */
 
         this.text.setText([
-            'x: '       + Math.round(this.player[0].x*100)/100,
-            'y: '       + Math.round(this.player[0].y*100)/100,
-            'accel: '   + Math.round(accel*100)/100,
-            'fps: '     + Math.round(this.game.loop.actualFps*100)/100,
-            'time: '    + Math.round(time*100)/100,
-            'delta: '   + Math.round(delta*100)/100
+            'x: '       + round(this.player[0].x),
+            'y: '       + round(this.player[0].y),
+            'v: '       + round(vel.x) + ', ' + round(vel.y),
+            'a: '       + round(accel.x) + ', ' + round(accel.x),
+            'velocity: '+ round(vel.length()),
+            'accel: '   + round(accel.length()),
+            'fps: '     + round(this.game.loop.actualFps),
+            'time: '    + round(time),
+            'delta: '   + round(delta)
         ]);
     }
 
